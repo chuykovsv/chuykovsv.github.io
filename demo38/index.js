16f4d14d350a0200e11e8654aef6e8f38ccab887
None <none@gmail.com>
2022-04-25 13:27:32 +0300

    demo38

---
+++ demo38/index.js
@@ -0,0 +1,252 @@
+const State = {
+    PENDING: 'оплачено',
+    READY: 'доставлено',
+    BROKEN: 'смонтированно'
+};
+
+class App {
+    constructor() {
+        this.renderer = new THREE.WebGLRenderer({ antialias:true });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setClearColor(0, 0);
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        this.renderer.toneMapping = THREE.LinearToneMapping
+        this.renderer.toneMappingExposure = 0.1;
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.appendChild(this.renderer.domElement);
+
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+        this.camera.position.set(20, 10, 20);
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+
+        this.raycaster = new THREE.Raycaster();
+        this.ndc = new THREE.Vector2();
+
+        this.role = 0;
+
+        this.selectObj = null;
+
+        this.nodes = new Map();
+
+        this.loadingCount = 0;
+
+        window.onresize = e => {
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        }
+
+        document.body.addEventListener('click', (e) => {
+            if (this.role === 0) { return; }
+
+            this.ndc.set(e.clientX / window.innerWidth * 2 - 1, 1 - e.clientY / window.innerHeight * 2);
+            this.raycaster.setFromCamera(this.ndc, this.camera);
+            const intersects = this.raycaster.intersectObjects(this.scene.children);
+
+            this._unselect();
+
+            if (intersects.length > 0) {
+                const obj = this.nodes.get(intersects[0].object.parent.name);
+                if (obj !== undefined) {
+                    if (this.role === 1){
+                        for (const el of document.getElementsByClassName('switcher-button')) {
+                            el.style.display = 'block';
+                        }
+                    }
+                    obj.material.color.setRGB(0, 0, 0);
+                    this.selectObj = obj;
+                    document.getElementById('name').innerHTML = intersects[0].object.parent.name + '<br>[' + obj.state + ']';
+                }
+            }
+        });
+
+        this._createUI();
+
+        const loop = () => {
+            this.controls.update();
+            this.renderer.render(this.scene, this.camera);
+            requestAnimationFrame(loop);
+        };
+
+        requestAnimationFrame(loop);
+    }
+
+    _unselect() {
+        document.getElementById('name').innerText = '';
+        if (this.selectObj !== null) {
+            const color = Math.random() * 0.3 + 0.7;
+            switch (this.selectObj.state) {
+                case State.PENDING:
+                    this.selectObj.material.color.setScalar(color);
+                    break;
+                case State.READY:
+                    this.selectObj.material.color.setRGB(color, color, 0);
+                    break;
+                case State.BROKEN:
+                    this.selectObj.material.color.setRGB(0, color, 0);
+                    break;
+            }
+            this.selectObj = null;
+        }
+
+        for (const el of document.getElementsByClassName('switcher-button')) {
+            el.style.display = 'none';
+        }
+    }
+
+    _createUI() {
+        const switcherDiv = document.createElement('div');
+        switcherDiv.id = 'switcher';
+        switcherDiv.innerText = 'Выберите деталь';
+        document.body.appendChild(switcherDiv);
+
+        const switcherPendingDiv = document.createElement('div');
+        switcherPendingDiv.className = 'switcher-button';
+        switcherPendingDiv.innerText = 'Оплачено';
+        switcherPendingDiv.style.background = '#aeaeae';
+        switcherPendingDiv.style.left = '0vw';
+        switcherDiv.appendChild(switcherPendingDiv);
+        switcherPendingDiv.addEventListener('click', (event) => {
+            event.stopPropagation();
+            this.selectObj.state = State.PENDING;
+            document.getElementById('name').innerHTML = this.selectObj.name + '<br>[' + this.selectObj.state + ']';
+            this._unselect();
+        });
+
+        const switcherReadyDiv = document.createElement('div');
+        switcherReadyDiv.className = 'switcher-button';
+        switcherReadyDiv.innerText = 'Доставлено';
+        switcherReadyDiv.style.background = '#aeae00';
+        switcherReadyDiv.style.left = '30vw';
+        switcherDiv.appendChild(switcherReadyDiv);
+        switcherReadyDiv.addEventListener('click', (event) => {
+            event.stopPropagation();
+            this.selectObj.state = State.READY;
+            document.getElementById('name').innerHTML = this.selectObj.name + '<br>[' + this.selectObj.state + ']';
+            this._unselect();
+        });
+
+        const switcherBrokenDiv = document.createElement('div');
+        switcherBrokenDiv.className = 'switcher-button';
+        switcherBrokenDiv.innerText = 'Смонтированно';
+        switcherBrokenDiv.style.background = '#00ae00';
+        switcherBrokenDiv.style.left = '60vw';
+        switcherDiv.appendChild(switcherBrokenDiv);
+        switcherBrokenDiv.addEventListener('click', (event) => {
+            event.stopPropagation();
+            this.selectObj.state = State.BROKEN;
+            document.getElementById('name').innerHTML = this.selectObj.name + '<br>[' + this.selectObj.state + ']';
+            this._unselect();
+        });
+
+        const nameDiv = document.createElement('div');
+        nameDiv.id = 'name';
+        nameDiv.innerText = '';
+        document.body.appendChild(nameDiv);
+
+        const rolesDiv = document.createElement('div');
+        rolesDiv.id = 'roles';
+        rolesDiv.innerText = 'Выберите роль';
+        document.body.appendChild(rolesDiv);
+
+        const editDiv = document.createElement('div');
+        editDiv.className = 'role-button';
+        editDiv.innerHTML = 'Строитель<br>(редактирование)'
+        rolesDiv.appendChild(editDiv);
+        editDiv.onclick = (event) => {
+            event.stopPropagation();
+            this.role = 1;
+            document.getElementById('roles').style.display = 'none';
+        }
+
+        const viewDiv = document.createElement('div');
+        viewDiv.className = 'role-button';
+        viewDiv.innerHTML = 'Снабженец<br>(просмотр)'
+        rolesDiv.appendChild(viewDiv);
+        viewDiv.onclick = (event) => {
+            event.stopPropagation();
+            this.role = 2;
+            document.getElementById('roles').style.display = 'none';
+        }
+
+        const loadingDiv = document.createElement('div');
+        loadingDiv.id = 'loading';
+        loadingDiv.innerText = 'Загрузка...';
+        document.body.appendChild(loadingDiv);
+    }
+
+    async load(url) {
+        const loader = new THREE.GLTFLoader();
+        loader.load(url, gltf => {
+            // gltf.scene.rotation.set(Math.PI * 0.5, 0, 0);
+            gltf.scene.position.set(-5, -5, -5);
+
+            gltf.scene.traverse(node => {
+                if (node.name.includes('NAUO') === false) { return; }
+
+                const material = new THREE.MeshStandardMaterial({
+                    color: new THREE.Color().setScalar(Math.random() * 0.3 + 0.7),
+                    roughness: 0.5,
+                    metalness: 0
+                });
+
+                const rnd = Math.random();
+                this.nodes.set(node.name, {
+                    name: node.name,
+                    material,
+                    state: rnd < 0.1 ? State.BROKEN : rnd < 0.5 ? State.READY : State.PENDING
+                });
+
+                node.traverse(mesh => {
+                    if (mesh instanceof THREE.Mesh) {
+                        mesh.material = material;
+                    }
+                });
+            });
+
+            this.scene.add(gltf.scene);
+
+            for (const obj of this.nodes.values()) {
+                const color = Math.random() * 0.3 + 0.7;
+                switch (obj.state) {
+                    case State.PENDING:
+                        obj.material.color.setScalar(color);
+                        break;
+                    case State.READY:
+                        obj.material.color.setRGB(color, color, 0);
+                        break;
+                    case State.BROKEN:
+                        obj.material.color.setRGB(0, color, 0);
+                        break;
+                }
+            }
+
+            this.loadingCount++;
+            if (this.loadingCount === 2) {
+                document.getElementById('loading').style.display = 'none';
+            }
+        });
+
+        const envLoader = new THREE.RGBELoader();
+        envLoader.load('./models/street.hdr', (texture) => {
+            texture.minFilter = THREE.NearestFilter;
+            texture.magFilter = THREE.NearestFilter;
+            texture.mapping = THREE.EquirectangularReflectionMapping;
+
+            this.scene.environment = texture;
+
+            this.loadingCount++;
+            if (this.loadingCount === 2) {
+                document.getElementById('loading').style.display = 'none';
+            }
+        });
+    }
+}
+
+window.onload = async () => {
+    const app = new App();
+
+    // await app.load('./models/MockUp-K1.gltf');
+    await app.load('./models/dom-stones.gltf');
+}
